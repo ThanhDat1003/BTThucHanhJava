b29f3111ce977a5911613809f286f7f11e8cf862
ThanhDat1003 <thanhdatt8910@gmail.com>
2022-01-17 16:39:50 +0700

    Create Bai1.java

---
+++ BaiThucHanh1/Bai1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+    public public static void public static void main(String[] args) throws Excaption {
+        Scanner sc = new Scanner(system.in);
+        String strHoTen = "";
+        system.out.println("Nhap ho ten: ")
+        strHoTen = sc.nextLine();
+        System.out.println("Xin chao" strHoTen);     
+    }
+
+}
