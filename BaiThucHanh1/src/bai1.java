ae725cdaa519eb092f7a3046a62e537aa1d2b1d7
ThanhDat1003 <thanhdatt8910@gmail.com>
2022-02-21 16:28:20 +0700

    a

--- BaiThucHanh1/src/bai1.java
+++ BaiThucHanh1/src/bai1.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class App {
+public class bai1 {
     public public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(system.in);
         String strHoTen = "";
-        system.out.println("Nhap ho ten: ")
+        system.out.println("Nhap ho ten: ");
         strHoTen = sc.nextLine();
         System.out.println("Xin chao" strHoTen);     
     }
