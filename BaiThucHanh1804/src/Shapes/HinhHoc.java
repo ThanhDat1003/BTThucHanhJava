713d286553dc9b494b86417ba262ec1ae9118f78
ThanhDat1003 <thanhdatt8910@gmail.com>
2022-04-18 18:01:16 +0700

    a

--- BaiThucHanh1804/src/Shapes/HinhHoc.java
+++ BaiThucHanh1804/src/Shapes/HinhHoc.java
@@ -1,3 +1,23 @@
+package Shapes;
+
 public class HinhHoc {
-    
+    public final float PI = 3.14f;
+
+    public String ten;
+    public Float chuVi;
+    public float dienTich;
+    public float theTich;
+
+    public void xuatTen() {
+        System.out.println("\n\n=====" + ten + "=====");
+    }
+    public void inChuvi() {
+        System.out.println("Chu vi = " + chuVi);
+    }
+    public void inDienTich() {
+        System.out.println("Dien tich = " + dientich);
+    }
+    public void inTheTich() {
+        System.out.println("The tich = " + theTich);
+    }
 }
--- BaiThucHanh1804/src/Shapes/HinhTru.java
+++ BaiThucHanh1804/src/Shapes/HinhTru.java
@@ -1,3 +1,14 @@
-public class HinhTru {
+package Shapes;
+
+import java.util.Scanner;
+
+public class HinhTru extends HinhTron {
+
+    public float chieuCao;
+
+    public HinhTru() {
+        ten = "Hình Trụ";
+    }
+
     
 }
